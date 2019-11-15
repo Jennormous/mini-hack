0a82c9c26f7b2ae91358dda67a97bceccfcd1e39
samuelimpey <51298345+samuelimpey@users.noreply.github.com>
2019-11-15 14:32:29 -0800

    retrieved data from API

--- client/src/Right/info.js
+++ client/src/Right/info.js
@@ -1,1 +1,14 @@
-// main div/background
+import React, { Component } from "react";
+import Class from "./class";
+import Race from "./race";
+
+export default class info extends Component {
+  render() {
+    return (
+      <div>
+        <Class />
+        <Race />
+      </div>
+    );
+  }
+}
